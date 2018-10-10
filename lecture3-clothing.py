2fd73def5cd1c0e432ffa458f027d9f6f5ed94a5
Sanjay Krishnan <sanjaykrishn@gmail.com>
2018-10-10 09:18:52 -0500

    first commit

---
+++ lecture3-clothing.py
@@ -0,0 +1,22 @@
+
+rain = True
+wind = True
+#two booleans
+
+if rain and wind:
+    
+    print("You should wear a jacket!")
+
+else:
+    
+    if rain and (not wind):
+
+        print ("An umbrella is probably fine!")
+
+    else: 
+
+        print("It's a sunny day!")
+
+print("test")
+
+
---
+++ lecture3-clothing2.py
@@ -0,0 +1,19 @@
+
+rain = True
+wind = False
+
+if rain and wind:
+    
+    print("You should wear a jacket!")
+
+else:
+    
+    if rain:
+
+        print ("An umbrella is probably fine!")
+
+    else: 
+
+        print("It's a sunny day!")
+
+
